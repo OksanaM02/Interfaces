4afb35a86f5435963e4b65a15898befbb34e273c
OksanaM02 <120195456+OksanaM02@users.noreply.github.com>
2024-02-16 20:20:09 +0100

    cositas

---
+++ Tailwind/src/main.js
@@ -0,0 +1,37 @@
+const data = [
+    {
+      'category': 'main_score',
+      'score': 76
+    },
+    {
+      "category": "Reaction",
+      "score": 80
+    },
+    {
+      "category": "Memory",
+      "score": 92
+    },
+    {
+      "category": "Verbal",
+      "score": 61
+    },
+    {
+      "category": "Visual",
+      "score": 72
+    }
+  ];
+  
+  var j = 1;
+  data.forEach(element => {
+    setTimeout(() => {
+      var i = 0;
+      var n = setInterval(inc);
+      function inc() {
+        document.getElementById(element.category).innerHTML = ++i;
+        if (i == element.score){
+          clearInterval(n)
+        }
+      }
+    }, 100 * j);
+    ++j
+  })
--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+}, 
 
+tailwind.config = {
+  theme: {
+    fontFamily: {
+        Hanken: '',
+      },
+    extend: {
+      colors: {
+        LightRed: "hsl(0, 100%, 67%)",
+        OrangeyYellow: "hsl(39, 100%, 56%)",
+        GreenTeal: "hsl(166, 100%, 37%)",
+        CobaltBlue: "hsl(234, 85%, 45%)",
+
+        White: "hsl(0, 0%, 100%)",
+        PaleBlue: "hsl(221, 100%, 96%)",
+        LightLavender: "hsl(241, 100%, 89%)",
+        DarkGrayBlue: "hsl(224, 30%, 27%)",
+
+        LightSlateBlueBackground: "hsl(252, 100%, 67%)",
+        LightRoyalBlueBackground: "hsl(241, 81%, 54%)",
+        VioletBlueCircle: "hsla(256, 72%, 46%, 1)",
+        PersianBlueCircle: "hsla(241, 72%, 46%, 0)",
+      },
+    },
+  },
+};
